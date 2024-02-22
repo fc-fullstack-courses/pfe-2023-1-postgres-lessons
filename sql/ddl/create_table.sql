a693b7eb97aa17ffa557d0347792b24bfc6d397f
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-22 19:15:54 +0200

    Add UNIQUE constraint examples

--- sql/ddl/create_table.sql
+++ sql/ddl/create_table.sql
@@ -29,24 +29,29 @@ CREATE TABLE users(
   -- опис структури таблиці
   -- uuid - специфічна варіація айдішника
   -- id UUID DEFAULT gen_random_uuid(),
-  id SERIAL NOT NULL,
+  id SERIAL NOT NULL UNIQUE,
   first_name VARCHAR(80) NOT NULL,
   last_name VARCHAR(80) NOT NULL,
   -- не дозволяє встановлювати у стовпчик значення NULL
+  -- обмеження стовпчика UNIQUE
+  -- email VARCHAR(160) NOT NULL UNIQUE,
   email VARCHAR(160) NOT NULL,
   balance NUMERIC(13,2) NOT NULL DEFAULT 0.00,
   is_male BOOLEAN,
   birthday DATE NOT NULL,
   created_at TIMESTAMP DEFAULT current_timestamp,
-  updated_at TIMESTAMP DEFAULT current_timestamp
+  updated_at TIMESTAMP DEFAULT current_timestamp,
+  -- обмеження таблиці
+  UNIQUE (email)
 );
 
 INSERT INTO users 
-(first_name, balance)
+(first_name, last_name, email, is_male, balance, birthday)
 VALUES
-('second', 1500),
-('third', DEFAULT);
-
+( 'second', 'last', 'mail@mail.com', true,-150000000,'2005-12-28' ),
+( '', '', 'test@test.com', true,1500,'2005-12-28' ),
+( 'second', 'last', '',true,1500, '1567-12-28' ),
+( 'second', 'last', 'mail@mail.com', true,1500,'2005-12-28' );
 /*
 
   15
@@ -56,3 +61,15 @@ VALUES
   1000.00000000000000000001
 
 */
+
+CREATE TABLE test (
+  a TEXT,
+  b TEXT,
+  -- унікальна група стовпчиків
+  UNIQUE (a, b)
+);
+
+INSERT INTO test
+(a, b)
+VALUES
+('1234','lorem');
