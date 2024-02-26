0ea7af1acb488f3c96fedd7157745ef679586098
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-26 19:32:50 +0200

    Add single user insertion script

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,31 @@ const config = {
 
 const client = new Client(config);
 
+const u = {
+  firstName: 'Inserted',
+  lastName: 'User',
+  email: 'ins_2@gmail.com',
+  isMale: true,
+  balance: 3501.55,
+  height: 1.70,
+  birthday: '1997-01-05'
+}
+
 async function start() {
   await client.connect();
 
-  const result = await client.query('SELECT * FROM users;');
+  const result = await client.query(`INSERT INTO users (
+    "first_name",
+    "last_name",
+    "email",
+    "is_male",
+    "balance",
+    "height",
+    "birthday"
+  ) 
+  VALUES
+  ('${u.firstName}', '${u.lastName}', '${u.email}', ${u.isMale}, ${u.balance}, ${u.height}, '${u.birthday}')
+  RETURNING *;`);
   // користувачі
   console.log(result.rows); 
 
