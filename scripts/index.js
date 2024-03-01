91a2869fd0f5bbbf12ca5f1e4c95679d09b14a10
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-01 16:28:20 +0200

    Expand DB generation script

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 const { Client } = require('pg');
 const fs = require('fs/promises');
+const _ = require('lodash');
 const { getUsers } = require('../api');
 const { mapUsers } = require('../utils/userUtils');
 const { user, password, host, port, database } = require('../config/db.json');
+const { chanceToMakeOrder } = require('../config/ordersGeneration.json');
+const { generateProducts } = require('../utils/products');
+const { generateOrders, generateProductsToOrders } = require('../utils/orders');
+const { generateReviewsMap, generateReviews, generateRatings } = require('../utils/reviews');
 
 const config = {
   user,
@@ -23,7 +28,7 @@ async function start() {
 
   await client.query(resetQuery);
 
-  const result = await client.query(`INSERT INTO users (
+  const { rows: createdUsers } = await client.query(`INSERT INTO users (
     "first_name",
     "last_name",
     "email",
@@ -37,8 +42,63 @@ async function start() {
   VALUES
   ${mapUsers(users)}
   RETURNING *;`);
-  // користувачі
-  console.log(result.rows);
+
+  const { rows: createdProducts } = await client.query(`
+    INSERT INTO products (
+      "name", 
+      "category", 
+      "description",
+      "manufacturer",
+      "price",
+      "quantity"
+    )
+    VALUES
+    ${generateProducts()}
+    RETURNING *
+    ;`);
+
+  const orderingUsers = createdUsers.filter(
+    () => _.random(0, 100) <= chanceToMakeOrder
+  );
+
+  const { rows: createdOrders } = await client.query(`
+    INSERT INTO orders (
+      "user_id",
+      "status"
+    )
+    VALUES
+    ${generateOrders(orderingUsers)}
+    RETURNING *
+    ;`);
+
+  const { rows: productsToOrdersResult } = await client.query(`
+    INSERT INTO products_to_orders (
+      product_id,
+      order_id,
+      quantity
+    ) VALUES
+    ${generateProductsToOrders({
+      orders: createdOrders,
+      products: createdProducts,
+    })}
+    RETURNING *
+    ;
+  `);
+
+  const reviewsMap = generateReviewsMap(createdOrders, productsToOrdersResult);
+
+  await client.query(`
+    BEGIN;
+    
+    INSERT INTO reviews
+    (user_id, product_id, rating_id, body)
+    VALUES ${generateReviews(reviewsMap)};
+    
+    INSERT INTO ratings
+    (review_id, rating)
+    VALUES ${generateRatings(reviewsMap)};
+    
+    COMMIT;`);
 
   await client.end();
 }
--- scripts/sql/reset.sql
+++ scripts/sql/reset.sql
@@ -1,4 +1,9 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS products_to_orders CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
@@ -14,3 +19,49 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP DEFAULT current_timestamp,
   updated_at TIMESTAMP DEFAULT current_timestamp
 );
+--
+CREATE TABLE IF NOT EXISTS orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES users,
+  status TEXT DEFAULT 'processing',
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+--
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(300) NOT NULL,
+  category VARCHAR(100),
+  description TEXT,
+  manufacturer TEXT,
+  price NUMERIC(13,2) NOT NULL CHECK (price > 0),
+  quantity INT NOT NULL CHECK (quantity >= 0),
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+--
+CREATE TABLE products_to_orders (
+  product_id INT NOT NULL REFERENCES products (id),
+  order_id INT NOT NULL REFERENCES orders (id),
+  quantity INT NOT NULL,
+  PRIMARY KEY (product_id, order_id)
+);
+--
+CREATE TABLE IF NOT EXISTS reviews (
+  id SERIAL PRIMARY KEY,
+  product_id int NOT NULL REFERENCES products,
+  user_id int NOT NULL REFERENCES users,
+  body text
+);
+--
+CREATE TABLE IF NOT EXISTS ratings (
+  id SERIAL PRIMARY KEY,
+  review_id int NOT NULL UNIQUE 
+  REFERENCES reviews ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  rating numeric (2,1)
+);
+--
+ALTER TABLE reviews
+ADD COLUMN rating_id int NOT NULL UNIQUE 
+REFERENCES ratings ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
